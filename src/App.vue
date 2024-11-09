f427df93ba6a25775526051ee71b2d632bfe91af
Craspouille <craspouille@craspouille>
2024-11-09 22:47:30 +0100

    premier commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navComponent :items="itemsNav" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navComponent from './components/nav_component.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    navComponent
+  },
+  data() {
+    return {
+      itemsNav: [
+        { "id": 1, "menu": 'Accueil', "menuUrl": '/accueil' },
+        { "id": 2, "menu": 'Qui sommes-nous ?', "menuUrl": '/about' },
+        { "id": 3, "menu": 'Nouveautés', "menuUrl": '/news' }
+      ]
+    }
   }
+
 }
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #111;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Pangolin, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
